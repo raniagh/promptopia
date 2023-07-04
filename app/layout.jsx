31a719c7c2c023ff8ebb936be4b11463388b4ff5
Your Name <rghzaiel@ggn-group.com>
2023-07-04 11:27:31 +0100

    design navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,25 @@
+import Nav from "@components/Nav";
 import "@styles/globals.css";
-const layout = () => {
-  return <div>layout</div>;
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
 };
 
-export default layout;
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
